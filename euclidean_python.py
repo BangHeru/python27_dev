f31340dd01d65c8faf24f9f44ff6a0f27a3e7094
BangHeru <bang.heru82@gmail.com>
2018-03-21 10:56:31 +0700

    Initial commit

---
+++ euclidean_python.py
@@ -0,0 +1,23 @@
+import numpy as np
+from numpy import linalg as LA
+from scipy import spatial 
+
+a = np.arange(9) - 4
+print a
+
+b = a.reshape((3, 3))
+print b
+
+print LA.norm(a)
+
+print LA.norm(b)
+
+print LA.norm(b, 'fro')
+
+#dist = np.linalg.norm(a-b)
+
+#print dist
+
+
+jarak = spatial.distance.euclidean(10,12)
+print "jarak : ", -jarak
