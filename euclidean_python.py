b5e71e21a1bd3f126e6d19fc42af089dbf4a80cb
BangHeru <bang.heru82@gmail.com>
2018-03-22 22:50:21 +0700

    add pso.py

--- euclidean_python.py
+++ euclidean_python.py
@@ -21,3 +21,9 @@ print LA.norm(b, 'fro')
 
 jarak = spatial.distance.euclidean(10,12)
 print "jarak : ", -jarak
+
+a = [2, 4, 6, 8]
+
+for i in a:
+	print "jarak : ", spatial.distance.euclidean(i, 4)
+
